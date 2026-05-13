83ed23e263ddff9eda662709bcfe793b8573b674
ZuqueCode <nicolas_z_araujo@estudante.sesisenai.org.br>
2026-05-13 07:55:48 -0300

    feat - adiconado todo código no javascript com dados para o calculo e caso haja erro no visor.

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function adicionar (valor){
 function limpar () {
     document.getElementById('visor').value = '';
 }
+
+
+
+function apagar() {
+    var visor = document.getElementById('visor');
+    visor.value = visor.value.slice(0, -1);
+}
+
+function calcular() {
+    var visor = document.getElementById('visor');
+    try{
+        
+        
+        visor.value = eval(visor.value);
+    } catch(error) {
+        
+        visor.value = 'Erro'
+    }
+}
